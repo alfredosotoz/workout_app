2759bf7d7aa56769eab127909d706aa9795cf38e
Alfredo <alfredosoto@outlook.com>
2017-10-12 17:34:05 +0000

    Display message if no exercises created

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -29,4 +29,14 @@ RSpec.feature "Listing exercises" do
     #expect(page).to have_content(@e3.workout)
     #expect(page).to have_content(@e3.workout_date)
   end
+  
+  scenario "shows no exercises if none created" do 
+    @john.exercises.delete_all
+    
+    visit '/'
+    
+    click_link 'My Lounge'
+    
+    expect(page).to have_content('No workouts yet')
+  end
 end
